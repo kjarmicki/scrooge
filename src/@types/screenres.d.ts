39f4be46077c5817868e56efabd96905529181f0
kjarmicki <jarmicki@gmail.com>
2019-01-06 15:19:21 +0100

    use whole space available on screen

---
+++ src/@types/screenres.d.ts
@@ -0,0 +1,4 @@
+declare module 'screenres' {
+  export type Res = [number, number];
+  export function get(): Res;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as puppeteer from 'puppeteer';
+import { get as getScreenResolution } from 'screenres';
 import Action from './actions/action';
 import configuration from './configuration';
 import createCredentials from './credentials';
@@ -8,8 +9,14 @@ import createCredentials from './credentials';
   const actionModule = await import(`./actions/${actionName}.ts`);
   const action: Action = actionModule.default;
   const credentials = createCredentials(configuration);
+  const [width, height] = getScreenResolution();
   const browser = await puppeteer.launch({
+    args: [`--window-size=${width},${height}`],
     headless: false
+  });
+  browser.on('targetcreated', async target => {
+    const page: any = await target.page();
+    await page._client.send('Emulation.clearDeviceMetricsOverride');
   });
   await action(browser, credentials);
 })();
